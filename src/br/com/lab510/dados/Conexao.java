d04ecd6cd445993b3e0d133182794d0395abb650
Leonardo Lopes <leolopes4@gmail.com>
2018-01-09 23:14:54 -0200

    Criada classe de comunicacao com o banco

--- src/br/com/lab510/dados/Conexao.java
+++ src/br/com/lab510/dados/Conexao.java
@@ -1,5 +1,29 @@
 package br.com.lab510.dados;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Conexao {
+	
+	private static final String USUARIO = "system";
+	private static final String SENHA = "Le@2uzumaki";
+	private static final String DRIVER = "oracle.jdbc.driver.OracleDriver";
+	private static final String URL = "jdbc:oracle:thin:@127.0.0.1:1521:xe";
+	
+	public static Connection abreConexaoComOBanco () {
+		
+		try {
+			Class.forName(DRIVER);
+			Connection conn = DriverManager.getConnection(URL, USUARIO, SENHA);
+		
+			return conn;
+			
+		}catch(SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return null;
+	
 
 }
