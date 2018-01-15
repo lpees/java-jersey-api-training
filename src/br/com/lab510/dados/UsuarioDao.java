c8954fbe9e3eb3c517c870382818579b49f82950
Leonardo <leolopes4@gmail.com>
2018-01-15 08:07:07 -0200

    Nova classe inserida UsuarioDao.java

---
+++ src/br/com/lab510/dados/UsuarioDao.java
@@ -0,0 +1,44 @@
+package br.com.lab510.dados;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class UsuarioDao {
+
+	public void salvaUsuarioNaBase(Usuario usuario) {
+		Connection conexaoOracle = Conexao.abreConexaoComOBanco();
+
+		try {
+			conexaoOracle.setAutoCommit(false);
+			StringBuilder sql = new StringBuilder();
+			sql.append("INSERT INTO USUARIO(");
+			sql.append("ID, NOME, SOBRENOME, EMAIL, CPF");
+			sql.append("VALUES (SQC_ID.NEXTVAL, ?, ?, ?, ?");
+
+			PreparedStatement inserir = conexaoOracle.prepareStatement(sql.toString());
+
+			inserir.setString(1, usuario.getNome());
+			inserir.setString(1, usuario.getSobrenome());
+			inserir.setString(1, usuario.getEmail());
+			inserir.setLong(1, usuario.getCPF());
+
+			inserir.executeUpdate();
+
+			conexaoOracle.commit();
+		} catch (SQLException e) {
+			conexaoOracle.rollback();
+			e.printStackTrace();
+		} finally {
+			if (conexaoOracle != null)
+			try {
+				conexaoOracle.close();
+			}catch (Exception e) {
+				e.printStackTrace();
+			}
+			
+
+		}
+	}
+
+}
