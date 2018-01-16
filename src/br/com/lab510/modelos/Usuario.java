dfed564982e3274db03650b66f575537c4e1f750
Leonardo <leolopes4@gmail.com>
2018-01-16 17:01:50 -0200

    Commit

---
+++ src/br/com/lab510/modelos/Usuario.java
@@ -0,0 +1,42 @@
+package br.com.lab510.modelos;
+
+public class Usuario {
+	
+	private String nome, sobrenome, email;
+	private long cpf;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSobrenome() {
+		return sobrenome;
+	}
+
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	
+	
+
+}
